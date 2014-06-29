6bffb850db8470cc2d95d072aaa2b609bb73d86a
Christian Smorra <smorra.christian@gmail.com>
2014-06-29 17:32:37 +0200

    how do i triforce?

---
+++ app.js
@@ -0,0 +1,51 @@
+var fs = require("fs");
+var _ = require("underscore");
+
+
+replaceUmlauts = function(s) {
+    var tr = {"ä":"ae", "ü":"ue", "ö":"oe", "ß":"ss" }
+    for(var letter in tr){
+        s = s.replace(letter, tr[letter]);
+    }
+    return  s //s.replace(/[äöüß]/g, function($0) { return tr[$0] })
+}
+
+
+
+var xkcdPW = function(){
+    var numWords = 3;
+    var seperator = ["+",","," ","_", ".", "/"]
+    var concatPw = []
+    var maxWordlen = 8;
+    var insertNum = true;
+    var wordList, lnCount, usedSeperator;
+
+    fs.readFile('./openthesaurus.txt', {encoding: "utf8"}, function(err, bunchOfWords){
+        if (err) throw err;
+
+        var bunchOfWords = bunchOfWords.replace("\n", "");
+
+        wordList = bunchOfWords.split(";");
+        lnCount = wordList.length;
+        usedSeperator = _.sample(seperator);
+
+        while(concatPw.length < numWords){
+            var idx = Math.floor(Math.random() * lnCount);
+            var w = wordList[idx];
+            w = replaceUmlauts(w);
+            if(w.length <= maxWordlen){
+                concatPw.push(w)
+            }
+        }
+        if(insertNum){
+            concatPw.push(_.random(0,100))
+        }
+
+        concatPw = _.shuffle(concatPw);
+        console.log(concatPw.join(usedSeperator))
+
+    })
+}
+
+
+_(1000).times(function(){xkcdPW()})
